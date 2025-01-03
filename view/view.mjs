d9b34d78a838aae01b806e7293ab2a028d2992f2
Hanuel José Martínez Guzmán <hanuel345martinez@gmail.com>
2025-01-03 01:33:49 -0400

    Agregando funcionalidad al boton del nav

--- view/view.mjs
+++ view/view.mjs
@@ -1,5 +1,18 @@
 
 
+const $ = element => document.querySelector(element);
+const $$ = element => document.querySelectorAll(element);
 
+const $btnNav = $(".menu-btn");
 
+$btnNav.addEventListener("click", () => {
+  let $nav = $(".nav");
+  if ($nav.classList.contains('open')) {
+    $nav.classList.remove('open');
+    $nav.style.display = "none";
+  } else {
+    $nav.classList.add('open');
+    $nav.style.display = "flex";
+  }
+});
 
